c06aec6b4fce758052b3e9d4e20c0e1fe00d0927
osdwebdev <osdwebdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-08 18:16:24 +0000

    Plugin Launch
    git-svn-id: https://plugins.svn.wordpress.org/osd-exclude-from-search-results/trunk@910656 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ osd_exclude_from_search_results.php
@@ -0,0 +1,73 @@
+<?php
+	/*
+	Plugin Name: OSD Exclude From Search Results
+	Plugin URI: http://outsidesource.com
+	Description: A plugin that excludes selected pages or posts from the search results.
+	Version: 1.0
+	Author: OSD Web Development Team
+	Author URI: http://outsidesource.com
+	License: GPL2v2
+	*/
+	
+	//register meta box to be able to use page as footer for ease of management
+	function osd_exclude_from_search_box_add() {
+		//add_meta_box( $id, $title, $callback, $page, $context, $priority, $callback_args ); 
+		add_meta_box('osd_exclude_from_search', 'OSD Exclude From Search', 'osd_efs_cb', 'page', 'side', 'default');
+		add_meta_box('osd_exclude_from_search', 'OSD Exclude From Search', 'osd_efs_cb', 'post', 'side', 'default');
+	}
+	add_action('add_meta_boxes', 'osd_exclude_from_search_box_add');
+	
+	//custom metabox call back
+	function osd_efs_cb() {
+		global $post;
+		$values = get_post_meta($post->ID);
+		$selected = ($values['exclude_from_search'][0] == 1) ? "checked='checked'" : '';
+		
+    	echo "<label for='exclude_from_search'>Exclude this page / post from the site search results? </label>";
+    	echo "<input type='checkbox' ".$selected." name='exclude_from_search' id='exclude_from_search' />";
+	}
+	
+	//save our custom page as footer box
+	function osd_exclude_from_search_box_save($post_id) {
+		// Bail if we're doing an auto save
+		if(defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) { 
+			return;
+		}
+		
+		// if our current user can't edit this post, bail
+		if(!current_user_can('edit_post')) {
+			return;
+		}
+
+		// Make sure your data is set before trying to save it
+		if(isset($_POST['exclude_from_search'])) {
+			$checked = 1;
+		} else {
+			$checked = 0;	
+		}
+		
+		update_post_meta($post_id, 'exclude_from_search', $checked);
+	}
+	add_action('save_post', 'osd_exclude_from_search_box_save');
+	
+	//remove any posts / pages from the search results that are marked to be excluded
+	function osd_exclude_from_search_filter($query) {
+		if($query->is_search && !is_admin()) {
+			global $wpdb;
+			$sql = "SELECT * 
+					FROM wp_postmeta 
+					WHERE meta_value = 1
+					AND meta_key = 'exclude_from_search'";
+					
+			$results = $wpdb->get_results($sql, ARRAY_A);
+			
+			foreach($results as $result) {
+				$excludeArray[] = $result['post_id'];
+			}
+			
+			$query->set('post__not_in', $excludeArray);
+		}
+		return $query;
+	}
+	add_filter('pre_get_posts','osd_exclude_from_search_filter');
+?>
---
+++ readme.txt
@@ -0,0 +1,52 @@
+=== Plugin Name ===
+Contributors: osdwebdev
+Tags: wordpress, search results, exclude from search, posts, pages, hide from search
+Requires at least: 3.4
+Tested up to: 3.9
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+OSD Exclude From Search Results plugin allows you to check a box on the edit screen for a page or post that will exclude it in your site search results.
+
+== Description ==
+
+OSD Exclude From Search Results plugin allows you to quickly remove a page or blog post from showing up in your site's search results.  This only changes the search results for the end user, not in the admin pages. The plugin adds a simple check box to the edit / create post screen to remove the post or page from the search results. It is extremely light weight, only 60 lines of code including the checkbox, so your site search will not slow down at all.
+
+== Installation ==
+
+1. Upload the osd-exclude-from-search-results directory to your `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Edit the page that you want to remove from the search, and check the new box
+
+== Frequently Asked Questions ==
+
+= Soon to come? =
+
+Yes, as users ask us questions.
+
+== Screenshots ==
+
+1. Edit Post Screen with Checkbox
+
+== Changelog ==
+
+= 1.0 =
+* Initial creation of the Exclude from Search Results plugin
+
+== Upgrade Notice ==
+
+= 1.0 =
+Adds quick and easy search result control
+
+== A brief Feature List ==
+
+1. Removes posts from search results
+2. Removes pages from search results
+3. Lightweight
+4. Can be modified with one line of code to include custom post types
+
+Link to plugin page [Wordpress plugin page](http://wordpress.org/plugins/osd-simple-table/ "Link").
+
+[markdown syntax]: http://daringfireball.net/projects/markdown/syntax
+            "Markdown is what the parser uses to process much of the readme file"
